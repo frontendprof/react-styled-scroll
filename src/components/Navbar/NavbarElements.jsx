a1291ca6b059e9b2928a65dc00221bbd23c64889
frontendprof <abdumaliksharipov@gmail.com>
2020-11-27 20:20:44 +0500

    Navbar component NavBtnLink

--- src/components/Navbar/NavbarElements.jsx
+++ src/components/Navbar/NavbarElements.jsx
@@ -1,13 +1,16 @@
 
 import styled from "styled-components";
 import { Link as LinkR } from "react-router-dom";
+import { Link as LinkS } from "react-router-dom";
+
+
 
 
 
 
 
 export const Nav = styled.nav`
-    background:#160f29;
+    background:#2c497f;
     height:80px;
     /* margin-top:-80px; */
     display:flex;
@@ -35,7 +38,7 @@ export const NavbarContainer = styled.div`
 `;
 
 export const NavLogo = styled(LinkR)`
-    color:#fff;
+    color:gold;
     cursor:pointer;
     justify-self:flex-start;
     font-size:1.5rem;
@@ -43,6 +46,82 @@ export const NavLogo = styled(LinkR)`
     align-items:center;
     margin-left:24px;
     font-weight:bold;
+    text-decoration:none;    
+`;
+
+
+export const MobileIcon = styled.div`
+    display:none;
+
+    @media screen and (max-width:768px){
+        display:block;
+        position:absolute;
+        top:0;
+        right:0;
+        transform:translate(-100%,60%);
+        font-size:1.8rem;
+        cursor:pointer;
+        color:#fff;
+    }
+`;
+
+
+export const NavMenu = styled.ul`
+    display:flex;
+    align-items:center;
+    list-style:none;
+    text-align:center;
+    margin-right:--22px;
+
+    @media screen and (max-width:768px){
+        display:none;
+    }
+`;
+
+
+export const NavItem = styled.li`
+    height:80px;
+`;
+
+
+export const NavLinks = styled(LinkS)`
+    color:yellow;
+    cursor:pointer;
+    padding: 0 1rem;
+    height:100%;
+    display:flex;
+    align-items:center;
     text-decoration:none;
     
+    &.active{
+        border-bottom:3px solid #01bf71;
+    }
+`;
+
+export const NavBtn = styled.nav`
+    display:flex;
+    align-items:center;
+
+    @media screen and (max-width:768px){
+        display:none;
+    }
+`
+
+export const NavBtnLink = styled(LinkR)`
+    border-radius:50px;
+    background:#baff29;
+    white-space:nowrap;
+    padding:10px 22px;
+    color:#d90368;
+    font-size:16px;
+    outline:none;
+    border:none;
+    cursor:pointer;
+    transition:all .2s ease-in-out;
+    text-decoration:none;
+
+    &:hover{
+        background:#fff;
+        color:#010606;
+    }
 `
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,8 +3,15 @@ import React from 'react';
 import {
     Nav,
     NavbarContainer,
-    NavLogo
+    NavLogo,
+    MobileIcon,
+    NavMenu,
+    NavItem,
+    NavLinks,
+    NavBtn,
+    NavBtnLink
 } from "./NavbarElements";
+import { FaBars } from "react-icons/fa";
 
 
 
@@ -12,13 +19,43 @@ import {
 
 const Navbar = () => {
     return (
-        <Nav>
-            <NavbarContainer>
-                <NavLogo>
-                    ABD-Shs
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo>
+                        ABD-Shs
                 </NavLogo>
-            </NavbarContainer>
-        </Nav>
+
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+
+                    <NavMenu>
+
+                        <NavItem>
+                            <NavLinks to="about">About</NavLinks>
+                        </NavItem>
+
+                        <NavItem>
+                            <NavLinks to="discover">Discover</NavLinks>
+                        </NavItem>
+
+                        <NavItem>
+                            <NavLinks to="services">Services</NavLinks>
+                        </NavItem>
+
+                        <NavItem>
+                            <NavLinks to="signup">Sign Up</NavLinks>
+                        </NavItem>
+
+                    </NavMenu>
+
+                    <NavBtn>
+                        <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </>
     )
 }
 
